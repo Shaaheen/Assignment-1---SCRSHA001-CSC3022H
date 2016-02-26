b9a9fff1427b59570efb965f2e9200dced21935b
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2016-02-27 01:55:17 +0200

    Cleaned up interface
    Signed-off-by: Shaaheen Sacoor <sacoorshaheen@gmail.com>

--- DatabaseUtils.cpp
+++ DatabaseUtils.cpp
@@ -14,93 +14,116 @@
 using namespace std;
 namespace SCRSHA001{
 
+    /*
+     *  Method to set up the vector as a database from the information stored in a database text file
+     */
     vector<StudentRecord> setUpDatabaseFromFile(){
-        cout << "function setUpDatabaseFromFile()called" << endl;
-        std::vector<StudentRecord> studentRecord;
+        cout << "function setUpDatabaseFromFile()called" << endl; //stub
+        std::vector<StudentRecord> studentRecord; //Vector container
 
-        ifstream MyFile("StudentDatabase.txt");
+        ifstream MyFile("StudentDatabase.txt"); //Try access txt database
         if (!MyFile) {
             //If file not created then create it
             std::ofstream outfile ("StudentDatabase.txt");
-
-           // outfile << "my text hdvn vd jknnjere!" << std::endl;
-
             outfile.close();
         }
 
-        ifstream MyFile1("StudentDatabase.txt");
+        ifstream MyFile1("StudentDatabase.txt"); //Access the txt database
         string line;
-        while ( getline(MyFile1,line) ){
-            //cout << line << "\n";
-            studentRecord = addFromDatabase(line,studentRecord);
-            //studentRecord.push_back(line);
+        while ( getline(MyFile1,line) ){ //Iterate over file line by line
+            studentRecord = addFromDatabase(line,studentRecord); //Get the student record from info in a line in txt file
         }
 
-        //cout << studentRecord.at(0).StudentNumber << endl;
         MyFile.close();
 
         return studentRecord;
     }
 
+    /*
+     *  Method to convert a StudentRecord struct to a string
+     */
     string studentRecordToString(StudentRecord student){
         return student.Name + " " + student.Surname + " " + student.StudentNumber +" " +
                student.classRecord;
     }
 
+    /*
+     *  Method to run through the vector database and get all student records in one string
+     */
     string queryDatabase(vector<StudentRecord> studentRecord ){
-        cout << "function queryDatabase(vector<StudentRecord> studentRecord)called" << endl;
+        cout << "function queryDatabase(vector<StudentRecord> studentRecord)called" << endl; //stub
         string records = "";
+
+        //Iterate over records and add string of the StudentRecord struct for each student
         for (StudentRecord student : studentRecord){
             records = records + studentRecordToString(student) + "\n";
-            //cout << student << endl;
         }
         return records;
     }
 
+    /*
+     * Method to take a string line from the database txt file and convert that line string into a Student record
+     * - Extracts the info from the database into the StudentRecord struct
+     */
     vector<StudentRecord> addFromDatabase(string studentInfo,vector<StudentRecord> stdRecords){
-        cout << "function addFromDatabase(string studentInfo, vector<StudentRecord> stdRecords)called" << endl;
-        stringstream ssin(studentInfo);
+        cout << "function addFromDatabase(string studentInfo, vector<StudentRecord> stdRecords)called" << endl; //stub
+        stringstream ssin(studentInfo); //create a string stream of the line holding student information from the db txt file
+        //Variables in struct
         string name;
         string surname;
         string studentNum;
         string classRecord = "";
 
+        //Place values from stream (line) into appropriate variables
         ssin >>name;
         ssin >>surname;
         ssin >> studentNum;
 
+        //The rest of the line is associated with the class record
         string item;
         while (ssin >> item){
             classRecord = classRecord + item + " ";
         }
-        classRecord = classRecord.substr(0,classRecord.size() - 1);
+        classRecord = classRecord.substr(0,classRecord.size() - 1); //Take away ending space
+
+        //Create a StudentRecord from the information extracted from the db txt file line
         StudentRecord studentFromDatabase {name, surname, studentNum, classRecord, 1};
         return add_student(studentFromDatabase,stdRecords);
 
     }
 
+    /*
+     *  Method to add a StudentRecord struct to the vector database
+     */
     vector<StudentRecord> add_student(StudentRecord studentRecord,std::vector<StudentRecord> studentRecords){
         studentRecords.push_back(studentRecord);
 
         return studentRecords;
     }
 
+    /*
+     *  Method to save the vector database to the database file
+     *  - Will overwrite database file incase there is a change on already existing data
+     */
     void saveDatabaseToFile(vector<StudentRecord> studentRecords){
-        cout << "function saveDatabaseToFile(vector<StudentRecord> studentRecords) called" << endl;
+        cout << "function saveDatabaseToFile(vector<StudentRecord> studentRecords) called" << endl; //sttub
         fstream  databaseFile;
-        databaseFile.open("StudentDatabase.txt", ios::out);
+        databaseFile.open("StudentDatabase.txt", ios::out); //Open db txt file for writing
         for (StudentRecord student:studentRecords){
-            databaseFile << studentRecordToString(student)<<endl;
+            databaseFile << studentRecordToString(student)<<endl; //write each student record to file
         }
         databaseFile.close();
     }
 
+    /*
+     *  Method to attempt to find a specific student (from student number) in the vector database
+     */
     StudentRecord findStudentInDatabase(std::string studentNum, std::vector<StudentRecord> stdRecords){
-        cout << "function findStudentInDatabase(std::string studentNum, std::vector<StudentRecord> stdRecords called" << endl;
+        cout << "function findStudentInDatabase(std::string studentNum, std::vector<StudentRecord> stdRecords called" << endl; //stub
         int index = 0;
-        for (StudentRecord student:stdRecords){
-            if (student.StudentNumber == studentNum){
-                student.canFind = index;
+        for (StudentRecord student:stdRecords){ //Iterate through vector container
+            if (student.StudentNumber == studentNum){ //if found student in db then return
+                student.canFind = index; //store the index of the position the student is found in database
                 return student;
             }
             index ++;
@@ -108,16 +131,19 @@ namespace SCRSHA001{
         return StudentRecord{"","","","",0};
     }
 
+    /*
+     *  Method to calculate the grade average of a student based on their class records
+     */
     double calculateGradeAverage(StudentRecord studentRecord){
-        stringstream stream(studentRecord.classRecord);
+        stringstream stream(studentRecord.classRecord); //stream from the class record string
         int totalMarks = 0;
         int numMarks = 0;
         int termMark;
         while (stream >> termMark){
-            totalMarks = totalMarks  + termMark;
-            numMarks ++;
+            totalMarks = totalMarks  + termMark; //add together all marks in the class record string
+            numMarks ++; //count the number of marks recorded
         }
-        return ((totalMarks+0.0)/numMarks);
+        return ((totalMarks+0.0)/numMarks); //divide total marks by number of marks to get average
     }
 
 
--- main.cpp
+++ main.cpp
@@ -13,30 +13,33 @@
 using namespace std;
 using namespace SCRSHA001;
 
-void clear(void){ system("clear");}
+void clear(void){ system("clear");} //To allow the console to clear
 
 int main() {
-    string choice;
-    std::vector<StudentRecord> studentRecord = setUpDatabaseFromFile();
+    string choice; //records the choice the user made
+    std::vector<StudentRecord> studentRecord = setUpDatabaseFromFile(); //Database vector which gets data from the database txt file
 
     while (true) {
         choice = "-1";
 
-        cout << "0: Add student" << endl;
-        cout << "1: Read database" << endl;
-        cout << "2: Save database" << endl;
-        cout << "3: display given student data" << endl;
-        cout << "4: Grade student" << endl;
+        //Basic UI interface
+        cout << "1: Add student" << endl;
+        cout << "2: Read database" << endl;
+        cout << "3: Save database" << endl;
+        cout << "4: Display given student data" << endl;
+        cout << "5: Grade student" << endl;
         cout << "q: Quit "<< endl;
         cout << "Enter a number (or q to quit) and press return... "<< endl;
-        cin >> choice;
+        cin >> choice; //record choice made
 
-        if (choice == "1"){
+        if (choice == "1"){ //Add a student
+            //Variables for the StudentRecord struct
             string name;
             string surname;
             string studentNum;
             string classRecord = "";
 
+            //Ask user to input variables for new student
             cout << "Name:" <<endl;
             cin >> name;
             cout << "Surname:" <<endl;
@@ -47,45 +50,48 @@ int main() {
             getline(cin,classRecord);
             getline(cin,classRecord);
 
-            int canfind = 1;
-            StudentRecord newStudent {name,surname,studentNum,classRecord,canfind};
+            int canfind = 1; //to show that student record should be accessible
+            StudentRecord newStudent {name,surname,studentNum,classRecord,canfind}; //Create new StudentRecord
 
+            //Will try to retrieve a record with the same student number
             StudentRecord checkIfAlreadyExists = findStudentInDatabase(studentNum,studentRecord);
-            if (checkIfAlreadyExists.canFind !=0){
-                studentRecord[checkIfAlreadyExists.canFind] = newStudent;
+            if (checkIfAlreadyExists.canFind !=0){ //if not 0, then it must exist
+                //canFind variable stores the position of a student record in the vector database
+                studentRecord[checkIfAlreadyExists.canFind] = newStudent; //replaces existing entry
             }
             else{
-                studentRecord = add_student(newStudent ,studentRecord);
+                studentRecord = add_student(newStudent ,studentRecord); //add new entry to database vector
             }
 
         }
         else if (choice == "2"){
-            cout << SCRSHA001::queryDatabase(studentRecord) <<endl;
+            cout << SCRSHA001::queryDatabase(studentRecord) <<endl; //get all records
         }
         else if (choice == "3"){
-            saveDatabaseToFile(studentRecord);
+            saveDatabaseToFile(studentRecord); //saves the vector database into a file
         }
         else if (choice == "4"){
             string studentNumSearch;
             cout << "Type in student number" <<endl;
             cin >> studentNumSearch;
+            //Attempts to find a student in the database based on their student number
             StudentRecord foundStudent = findStudentInDatabase(studentNumSearch,studentRecord);
-            if (foundStudent.canFind !=0){
-                cout << studentRecordToString(foundStudent) <<endl;
+            if (foundStudent.canFind !=0){ //if found
+                cout << studentRecordToString(foundStudent) <<endl; //print the record
             }
             else{
                 cout << "Student not found in database" << endl;
             }
 
         }
-        else if (choice == "5"){
+        else if (choice == "5"){ //grade average - similar as above
             string studentNumSearch;
             cout << "Type in student number" <<endl;
             cin >> studentNumSearch;
             StudentRecord foundStudent = findStudentInDatabase(studentNumSearch,studentRecord);
             if (foundStudent.canFind !=0){
                 cout << "The Grade Average of " << foundStudent.StudentNumber << " is: " <<endl;
-                cout << calculateGradeAverage(foundStudent) << endl;
+                cout << calculateGradeAverage(foundStudent) << endl; //Get the grade average
             }
             else{
                 cout << "Student not found in database" << endl;
